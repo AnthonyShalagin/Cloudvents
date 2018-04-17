5234a207a38312862f5036d63e42e4d4ead989f4
Anthony Shalagin <anthony.shalagin@gmail.com>
2018-04-17 14:38:18 -0400

    First stab at integrating meetup API

--- routes/routes.js
+++ routes/routes.js
@@ -1,42 +1,16 @@
 var faker = require("faker");
-
+var request = require('request');
 var appRouter = function (app) {
 
   app.get("/", function (req, res) {
-    res.status(200).send({ message: 'Welcome to our restful API' });
-  });
-
-  app.get("/user", function (req, res) {
-    var data = ({
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
-      username: faker.internet.userName(),
-      email: faker.internet.email()
+    // res.status(200).send({ message: 'Welcome to our restful API' });
+    request.get('http://api.meetup.com/2/cities', function(err, response, body) {
+        if (!err && response.statusCode == 200) {
+            var locals = JSON.parse(body);
+            res.send(locals);
+        }
     });
-    res.status(200).send(data);
   });
-
- app.get("/users/:num", function (req, res) {
-   var users = [];
-   var num = req.params.num;
-
-   if (isFinite(num) && num  > 0 ) {
-     for (i = 0; i <= num-1; i++) {
-       users.push({
-           firstName: faker.name.firstName(),
-           lastName: faker.name.lastName(),
-           username: faker.internet.userName(),
-           email: faker.internet.email()
-        });
-     }
-
-     res.status(200).send(users);
-    
-   } else {
-     res.status(400).send({ message: 'invalid number supplied' });
-   }
-
- });
 }
 
 module.exports = appRouter;
