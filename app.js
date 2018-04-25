b3182be90ebe80678b30c95b717cca2fcf15d91c
Anthony Shalagin <anthony.shalagin@gmail.com>
2018-04-25 11:16:45 -0400

    Initial handlebar config

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const bodyParser = require('body-parser');
 const routes = require('./routes/routes.js');
 const app = express();
 
+var exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/a', function (req, res) {
+    res.render('home');
+});
 
 routes(app);
 
